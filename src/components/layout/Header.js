9d588a5457fff0b3c1e80a09857bec3344b41ec2
Brandon Johnson <brandon121j@gmail.com>
2022-05-16 22:52:31 -0400

    Checkpoint commit

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import '../styles/Header.css';
-import { Layout as AntLayout } from 'antd';
-const { Header: AntHeader } = AntLayout;
+import { Row, Col, Input, Dropdown, Menu, Space, Button } from 'antd';
 
-const Header = () => (
-	<AntHeader className="navbar">
-		<div>
-		<div><h1>Brandon Music</h1></div>
-		<div></div>
-		<div></div>
-		</div>
-	</AntHeader>
-);
+const Header = () => {
+  const menu = (
+    <Menu
+      items={[
+        {
+          label: (
+            <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+              1st menu item
+            </a>
+          ),
+        },
+        {
+          label: (
+            <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+              2nd menu item
+            </a>
+          ),
+        },
+        {
+          label: (
+            <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
+              3rd menu item
+            </a>
+          ),
+        },
+      ]}
+    />
+  );
+
+  return (
+	<div>
+	<Row align='middle' className='navbar'>
+      <Col className="item1">Brandon Music</Col>
+      <Col className="item2"><Input placeholder="input search text" style={{ width: 500, backgroundColor: 'rgb(28, 28, 30)', color: 'rgb(229, 229, 234)' }} /></Col>
+      <Col className="item3"><Dropdown  placement="bottom" overlay={menu}><Button style={{ backgroundColor: 'inherit', color: 'inherit'}}>bottom</Button></Dropdown></Col>
+    </Row>
+	</div>
+  );
+  };
 
 export default Header;
